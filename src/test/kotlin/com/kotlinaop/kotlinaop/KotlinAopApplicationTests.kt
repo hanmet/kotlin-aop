dc161ad982507b51239b643af4f3b364911b034e
Hannes Methner <hannes.methner@outlook.com>
2022-12-10 07:13:28 +0100

    init

---
+++ src/test/kotlin/com/kotlinaop/kotlinaop/KotlinAopApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.kotlinaop.kotlinaop
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class KotlinAopApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
